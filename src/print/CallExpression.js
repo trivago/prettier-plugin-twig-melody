a9ba38f1f5547fee1700a9a85d55e550e6a2aa55
tbartel <thomas.bartel@trivago.com>
2019-09-18 11:25:38 +0200

    Fix bug in CallExpression printing

--- src/print/CallExpression.js
+++ src/print/CallExpression.js
@@ -1,14 +1,15 @@
 const prettier = require("prettier");
-const { group, concat, softline, indent, join } = prettier.doc.builders;
+const { group, concat, softline, line, indent, join } = prettier.doc.builders;
 
 const p = (node, path, print) => {
+    debugger;
     const mappedArguments = path.map(print, "arguments");
     return group(
         concat([
             node.callee.name,
             "(",
             softline,
-            indent(join(concat[(",", softline)], mappedArguments)),
+            indent(join(concat([",", line]), mappedArguments)),
             softline,
             ")"
         ])
