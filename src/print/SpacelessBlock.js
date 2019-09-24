fea1a09e70bb38091e8f78ef8dc6b165371dbcf0
tbartel <thomas.bartel@trivago.com>
2019-09-24 06:24:08 +0200

    Use util function to print children of SpacelessBlock Extract failing attribute from test case

--- src/print/SpacelessBlock.js
+++ src/print/SpacelessBlock.js
@@ -1,16 +1,10 @@
 const prettier = require("prettier");
 const { concat, softline, group, join, indent } = prettier.doc.builders;
-
-const printBodyWithChildren = (path, print) => {
-    const printedBody = path.map(print, "body");
-    const withoutEmptyStrings = printedBody.filter(s => s !== "");
-    const joinedContents = join(softline, withoutEmptyStrings);
-    return indent(concat([softline, joinedContents]));
-};
+const { printChildren } = require("../util");
 
 const p = (node, path, print) => {
     const opener = "{% spaceless %}";
-    const indentedBody = printBodyWithChildren(path, print);
+    const indentedBody = printChildren(path, print, "body");
     const result = group(
         concat([opener, indentedBody, softline, "{% endspaceless %}"])
     );
