a800385baedd998a932d74b4efcd5efe7a725128
tbartel <thomas.bartel@trivago.com>
2019-09-24 21:35:32 +0200

    Fix application of parser/lexer extensions

--- src/parser.js
+++ src/parser.js
@@ -1,36 +1,57 @@
 const { CharStream, Lexer, TokenStream, Parser } = require("melody-parser");
 const { extension: coreExtension } = require("melody-extension-core");
 
-const applyExtension = (parser, extension) => {
-    if (extension.tags) {
-        for (const tag of extension.tags) {
-            parser.addTag(tag);
+const createConfiguredLexer = (code, ...extensions) => {
+    const lexer = new Lexer(new CharStream(code));
+    for (const extension of extensions) {
+        if (extension.unaryOperators) {
+            lexer.addOperators(...extension.unaryOperators.map(op => op.text));
+        }
+        if (extension.binaryOperators) {
+            lexer.addOperators(...extension.binaryOperators.map(op => op.text));
         }
     }
-    if (extension.unaryOperators) {
-        for (const op of extension.unaryOperators) {
-            parser.addUnaryOperator(op);
+    return lexer;
+};
+
+const configureParser = (parser, ...extensions) => {
+    for (const extension of extensions) {
+        if (extension.tags) {
+            for (const tag of extension.tags) {
+                parser.addTag(tag);
+            }
         }
-    }
-    if (extension.binaryOperators) {
-        for (const op of extension.binaryOperators) {
-            parser.addBinaryOperator(op);
+        if (extension.unaryOperators) {
+            for (const op of extension.unaryOperators) {
+                parser.addUnaryOperator(op);
+            }
         }
-    }
-    if (extension.tests) {
-        for (const test of extension.tests) {
-            parser.addTest(test);
+        if (extension.binaryOperators) {
+            for (const op of extension.binaryOperators) {
+                parser.addBinaryOperator(op);
+            }
+        }
+        if (extension.tests) {
+            for (const test of extension.tests) {
+                parser.addTest(test);
+            }
         }
     }
 };
 
-const configureParser = parser => {
-    applyExtension(parser, coreExtension);
+const createConfiguredParser = (code, ...extensions) => {
+    if (!extensions.includes(coreExtension)) {
+        extensions = [coreExtension, ...extensions];
+    }
+    const parser = new Parser(
+        new TokenStream(createConfiguredLexer(code, ...extensions))
+    );
+    configureParser(parser, ...extensions);
+    return parser;
 };
 
 const parse = code => {
-    const parser = new Parser(new TokenStream(new Lexer(new CharStream(code))));
-    configureParser(parser);
+    const parser = createConfiguredParser(code, coreExtension);
     return parser.parse();
 };
 
