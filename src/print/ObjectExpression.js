768bd89b3843d7c000dee8d370e736029994b9b1
tbartel <thomas.bartel@trivago.com>
2019-09-24 08:10:15 +0200

    ObjectExpression: Add space between object contents and curly braces when printed on one line

--- src/print/ObjectExpression.js
+++ src/print/ObjectExpression.js
@@ -1,14 +1,14 @@
 const prettier = require("prettier");
-const { group, concat, softline, line, indent, join } = prettier.doc.builders;
+const { group, concat, line, indent, join } = prettier.doc.builders;
 
 const p = (node, path, print) => {
     const mappedElements = path.map(print, "properties");
     const indentedContent = concat([
-        softline,
+        line,
         join(concat([",", line]), mappedElements)
     ]);
 
-    return group(concat(["{", indent(indentedContent), softline, "}"]));
+    return group(concat(["{", indent(indentedContent), line, "}"]));
 };
 
 module.exports = {
