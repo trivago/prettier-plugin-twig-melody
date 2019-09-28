38a99f985db13fc2c6064b7a54b1ad56317655ba
tbartel <thomas.bartel@trivago.com>
2019-09-29 00:13:48 +0200

    FilterExpression: Implement manual descent instead of recursion Adapt FilterBlockStatement accordingly Update tests and snapshots

--- src/print/FilterBlockStatement.js
+++ src/print/FilterBlockStatement.js
@@ -1,27 +1,15 @@
 const prettier = require("prettier");
-const { concat, hardline } = prettier.doc.builders;
+const { concat, group, line, softline, hardline } = prettier.doc.builders;
 const { processChildExpressions } = require("../util");
-
-// "filter" is parsed as the target of the first
-// filter expression, which would output a "|" between "filter"
-// and the expression. We remove it.
-const removeFirstPipe = expression => {
-    let current = expression;
-    while (current.contents.parts[0] !== "filter") {
-        current = current.contents.parts[0];
-    }
-    current.contents.parts.splice(1, 1);
-};
 
 const p = (node, path, print) => {
     const parts = ["{% "];
     const printedExpression = path.call(print, "filterExpression");
-    removeFirstPipe(printedExpression);
-    parts.push(printedExpression, " %}");
+    parts.push(printedExpression, line, "%}");
     const printedBody = path.map(print, "body");
     parts.push(processChildExpressions(printedBody));
-    parts.push(concat([hardline, "{% endfilter %}"]));
-    return concat(parts);
+    parts.push(concat([softline, "{% endfilter %}"]));
+    return group(concat(parts));
 };
 
 module.exports = {
--- src/print/FilterExpression.js
+++ src/print/FilterExpression.js
@@ -1,5 +1,6 @@
 const prettier = require("prettier");
 const { group, concat, indent, line, softline, join } = prettier.doc.builders;
+const { Node } = require("melody-types");
 
 const printGroup = (prefix, elements, separator, suffix) => {
     return group(
@@ -12,18 +13,69 @@ const printGroup = (prefix, elements, separator, suffix) => {
     );
 };
 
-const p = (node, path, print) => {
-    const target = path.call(print, "target");
+const printOneFilterExpression = (node, path, print, nodePath) => {
     const hasArguments = node.arguments && node.arguments.length > 0;
-    const printedArguments = hasArguments ? path.map(print, "arguments") : [];
+    const printedArguments = hasArguments
+        ? path.map(print, ...nodePath, "arguments")
+        : [];
+    debugger;
     const args = hasArguments
         ? printGroup("(", printedArguments, concat([",", line]), ")")
         : "";
-    const filterName = path.call(print, "name");
+    const filterName = path.call(print, ...nodePath, "name");
+    return concat([filterName, args]);
+};
 
-    return group(
-        concat([target, " |", indent(concat([line, filterName, args]))])
-    );
+const joinFilters = filterExpressions => {
+    return join(concat([line, "| "]), filterExpressions);
+};
+
+const p = (node, path, print) => {
+    let currentNode = node;
+    const pathToFinalTarget = ["target"];
+    let filterExpressions = [printOneFilterExpression(node, path, print, [])];
+
+    // Here, we do not do the usual recursion using path.call(), but
+    // instead traverse the chain of FilterExpressions ourselves (in
+    // case there are multiple chained FilterExpressions, that is).
+    // Reason: For a proper layout like this
+    // "Some text"
+    //     | filter1
+    //     | filter2(arg)
+    //     | filter3
+    // we need all the individual filter expressions in one group. This
+    // can only be achieved by collecting them manually in the top-level
+    // FilterExpression node.
+    while (Node.isFilterExpression(currentNode.target)) {
+        filterExpressions.unshift(
+            printOneFilterExpression(
+                currentNode.target,
+                path,
+                print,
+                pathToFinalTarget
+            )
+        );
+        pathToFinalTarget.push("target"); // Go one level deeper
+        currentNode = currentNode.target;
+    }
+
+    const finalTarget = path.call(print, ...pathToFinalTarget);
+    const isFilterBlock = finalTarget === "filter"; // Special case of FilterBlockStatement
+    const parts = [finalTarget];
+    if (isFilterBlock) {
+        parts.push(concat([" ", filterExpressions[0]]));
+        filterExpressions = filterExpressions.slice(1);
+    }
+    if (filterExpressions.length > 0) {
+        const indentedFilters = concat([
+            line,
+            "| ",
+            joinFilters(filterExpressions)
+        ]);
+        parts.push(indent(indentedFilters));
+    }
+
+    return group(concat(parts));
 };
 
 module.exports = {
