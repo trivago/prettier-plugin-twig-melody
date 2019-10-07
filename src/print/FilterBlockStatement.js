820c06caebb56466e6c23500b3887bc6ae44831b
tbartel <thomas.bartel@trivago.com>
2019-10-07 06:20:11 +0200

    Improve whitespace handling for FilterBlockStatement

--- src/print/FilterBlockStatement.js
+++ src/print/FilterBlockStatement.js
@@ -1,15 +1,24 @@
 const prettier = require("prettier");
-const { concat, group, line, softline, hardline } = prettier.doc.builders;
-const { processChildExpressions } = require("../util");
+const { concat, group, line, indent, hardline } = prettier.doc.builders;
+const { removeSurroundingWhitespace, printChildGroups } = require("../util");
 
-const p = (node, path, print) => {
+const printOpeningGroup = (node, path, print) => {
     const parts = ["{% "];
     const printedExpression = path.call(print, "filterExpression");
     parts.push(printedExpression, line, "%}");
-    const printedBody = path.map(print, "body");
-    parts.push(processChildExpressions(printedBody));
-    parts.push(concat([softline, "{% endfilter %}"]));
     return group(concat(parts));
+};
+
+const p = (node, path, print) => {
+    const openingGroup = printOpeningGroup(node, path, print);
+
+    node.body = removeSurroundingWhitespace(node.body);
+    const childGroups = printChildGroups(node, path, print, "body");
+    const body = indent(group(concat([hardline, ...childGroups])));
+
+    const closingStatement = concat([hardline, "{% endfilter %}"]);
+
+    return concat([openingGroup, body, closingStatement]);
 };
 
 module.exports = {
