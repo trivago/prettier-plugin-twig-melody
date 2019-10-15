69d5219c9babb4b9e2400efd74f276f5eaa9a1ae
tbartel <thomas.bartel@trivago.com>
2019-10-15 09:37:57 +0200

    Enable proper grouping for BinaryExpression

--- src/print/BinaryExpression.js
+++ src/print/BinaryExpression.js
@@ -1,16 +1,47 @@
 const prettier = require("prettier");
-const { group, concat, line } = prettier.doc.builders;
+const { group, join, concat, line } = prettier.doc.builders;
+const { Node } = require("melody-types");
+
+const printOneOperand = (node, path, print, nodePath) => {
+    return concat([
+        line,
+        node.operator,
+        " ",
+        path.call(print, ...nodePath, "right")
+    ]);
+};
 
 const p = (node, path, print) => {
-    return group(
-        concat([
-            path.call(print, "left"),
-            " ",
-            node.operator,
-            line,
-            path.call(print, "right")
-        ])
-    );
+    let currentNode = node;
+    const pathToFinalLeftHandSide = ["left"];
+    const binaryExpressions = [printOneOperand(node, path, print, [])];
+
+    // Here, we do not do the usual recursion using path.call(), but
+    // instead traverse the chain of BinaryExpressions ourselves (in
+    // case there are multiple chained BinaryExpressions, that is).
+    // Reason: For a proper layout like this
+    // cond1 or
+    // cond2 or
+    // cond3 or
+    // cond4
+    // we need all the individual binary expressions in one group. This
+    // can only be achieved by collecting them manually in the top-level
+    // BinaryExpression node.
+    while (Node.isBinaryExpression(currentNode.left)) {
+        binaryExpressions.unshift(
+            printOneOperand(
+                currentNode.left,
+                path,
+                print,
+                pathToFinalLeftHandSide
+            )
+        );
+        pathToFinalLeftHandSide.push("left"); // Go one level deeper
+        currentNode = currentNode.left;
+    }
+    binaryExpressions.unshift(path.call(print, ...pathToFinalLeftHandSide));
+
+    return group(join("", binaryExpressions));
 };
 
 module.exports = {
