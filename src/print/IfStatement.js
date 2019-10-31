de963f57bc1d114f10d204eac99128e4066bcd4a
tbartel <thomas.bartel@trivago.com>
2019-10-31 05:50:32 +0100

    Fix IfStatement to also handle "elseif" cases

--- src/print/IfStatement.js
+++ src/print/IfStatement.js
@@ -1,14 +1,21 @@
 const prettier = require("prettier");
 const { group, indent, line, hardline, concat } = prettier.doc.builders;
 const { EXPRESSION_NEEDED, printChildBlock } = require("../util");
+const { Node } = require("melody-types");
+
+const IS_ELSEIF = Symbol("IS_ELSEIF");
 
 const p = (node, path, print) => {
     node[EXPRESSION_NEEDED] = false;
-    const hasAlternate =
+    const hasElseBranch =
         Array.isArray(node.alternate) && node.alternate.length > 0;
+    const hasElseIfBranch = Node.isIfStatement(node.alternate);
+    const isElseIf = node[IS_ELSEIF] === true;
+
     const ifClause = group(
         concat([
-            "{% if",
+            "{% ",
+            isElseIf ? "elseif" : "if",
             indent(concat([line, path.call(print, "test")])),
             line,
             "%}"
@@ -16,11 +23,19 @@ const p = (node, path, print) => {
     );
     const ifBody = printChildBlock(node, path, print, "consequent");
     const parts = [ifClause, ifBody];
-    if (hasAlternate) {
+    if (hasElseBranch) {
         parts.push(hardline, "{% else %}");
         parts.push(printChildBlock(node, path, print, "alternate"));
+    } else if (hasElseIfBranch) {
+        node.alternate[IS_ELSEIF] = true;
+        parts.push(hardline);
+        parts.push(path.call(print, "alternate"));
     }
-    parts.push(hardline, "{% endif %}");
+    // The {% endif %} will be taken care of
+    // by the final step in the recursion
+    if (!hasElseIfBranch) {
+        parts.push(hardline, "{% endif %}");
+    }
     return concat(parts);
 };
 
