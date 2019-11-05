f28d25b8c957d304440c0f9275b1ec8d85b0a1fd
tbartel <thomas.bartel@trivago.com>
2019-11-05 08:42:13 +0100

    Fix ObjectProperty to also take computed keys and have-to-quoted keys into account

--- src/print/ObjectProperty.js
+++ src/print/ObjectProperty.js
@@ -4,12 +4,19 @@ const { quoteChar } = require("../util");
 const { Node } = require("melody-types");
 
 const p = (node, path, print, options) => {
-    const parts = [
-        quoteChar(options),
-        path.call(print, "key"),
-        quoteChar(options),
-        ": "
-    ];
+    const parts = [];
+    if (node.computed) {
+        parts.push("(");
+    } else if (Node.isStringLiteral(node.key)) {
+        parts.push(quoteChar(options));
+    }
+    parts.push(path.call(print, "key"));
+    if (node.computed) {
+        parts.push(")");
+    } else if (Node.isStringLiteral(node.key)) {
+        parts.push(quoteChar(options));
+    }
+    parts.push(": ");
     if (Node.isStringLiteral(node.value)) {
         parts.push(quoteChar(options));
     }
