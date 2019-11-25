61e94a68f3c7982aa5bb5f6299636a790d4ef6bd
tbartel <thomas.bartel@trivago.com>
2019-11-25 20:34:21 +0100

    Fix test cases with ({ causing too many indentation levels

--- src/print/CallExpression.js
+++ src/print/CallExpression.js
@@ -5,18 +5,29 @@ const {
     STRING_NEEDS_QUOTES,
     wrapExpressionIfNeeded
 } = require("../util");
+const { Node } = require("melody-types");
 
 const p = (node, path, print) => {
     node[EXPRESSION_NEEDED] = false;
     node[STRING_NEEDS_QUOTES] = true;
     const mappedArguments = path.map(print, "arguments");
-    const parts = [
-        path.call(print, "callee"),
-        "(",
-        indent(concat([softline, join(concat([",", line]), mappedArguments)])),
-        softline,
-        ")"
-    ];
+    const parts = [path.call(print, "callee"), "("];
+    if (
+        node.arguments.length === 1 &&
+        Node.isObjectExpression(node.arguments[0])
+    ) {
+        // Optimization: No line break between "(" and "{" if
+        // there is exactly one object parameter
+        parts.push(mappedArguments[0], ")");
+    } else {
+        parts.push(
+            indent(
+                concat([softline, join(concat([",", line]), mappedArguments)])
+            ),
+            softline,
+            ")"
+        );
+    }
 
     wrapExpressionIfNeeded(path, parts, node);
 
