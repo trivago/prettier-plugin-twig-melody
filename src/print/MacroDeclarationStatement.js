a8ad6c56bb0ba51790c25817981fdbb8c8da2dfb
tbartel <thomas.bartel@trivago.com>
2019-12-18 06:37:34 +0100

    Whitespace trimming for macro statement

--- src/print/MacroDeclarationStatement.js
+++ src/print/MacroDeclarationStatement.js
@@ -10,18 +10,28 @@ const {
 } = prettier.doc.builders;
 
 const printOpener = (node, path, print) => {
-    const parts = ["{% macro ", path.call(print, "name"), "("];
+    const parts = [
+        node.trimLeft ? "{%-" : "{%",
+        " macro ",
+        path.call(print, "name"),
+        "("
+    ];
     const mappedArguments = path.map(print, "arguments");
     const joinedArguments = join(concat([",", line]), mappedArguments);
     parts.push(indent(concat([softline, joinedArguments])));
-    parts.push(")", line, "%}");
+    parts.push(")", line, node.trimRightMacro ? "-%}" : "%}");
     return group(concat(parts));
 };
 
 const p = (node, path, print) => {
     const parts = [printOpener(node, path, print)];
     parts.push(indent(concat([hardline, path.call(print, "body")])));
-    parts.push(hardline, "{% endmacro %}");
+    parts.push(
+        hardline,
+        node.trimLeftEndmacro ? "{%-" : "{%",
+        " endmacro ",
+        node.trimRight ? "-%}" : "%}"
+    );
     return concat(parts);
 };
 
