0efbc71b01e39c6d41a5b623c66e8504407e602f
tbartel <thomas.bartel@trivago.com>
2019-12-19 17:42:15 +0100

    Remove line break before /> in self-closing element

--- src/print/Element.js
+++ src/print/Element.js
@@ -19,7 +19,7 @@ const {
 const printOpeningTag = (node, path, print) => {
     const opener = "<" + node.name;
     const printedAttributes = printSeparatedList(path, print, "", "attributes");
-    const openingTagEnd = node.selfClosing ? concat([line, "/>"]) : ">";
+    const openingTagEnd = node.selfClosing ? " />" : ">";
     const hasAttributes = node.attributes && node.attributes.length > 0;
 
     if (hasAttributes) {
