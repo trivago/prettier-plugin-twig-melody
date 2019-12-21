da81ab374e9afd9e06715b945eddbedd8551f037
tbartel <thomas.bartel@trivago.com>
2019-12-21 07:27:34 +0100

    Respect operator precedence in binary expressions

--- src/print/BinaryExpression.js
+++ src/print/BinaryExpression.js
@@ -1,13 +1,21 @@
 const prettier = require("prettier");
-const { group, join, concat, line } = prettier.doc.builders;
+const { group, join, concat, line, indent } = prettier.doc.builders;
 const { Node } = require("melody-types");
 const {
     EXPRESSION_NEEDED,
     STRING_NEEDS_QUOTES,
     INSIDE_OF_STRING,
     wrapExpressionIfNeeded,
+    firstValueInAncestorChain,
     quoteChar
 } = require("../util");
+const { extension: coreExtension } = require("melody-extension-core");
+const ALREADY_INDENTED = Symbol("ALREADY_INDENTED");
+
+const operatorPrecedence = coreExtension.binaryOperators.reduce((acc, curr) => {
+    acc[curr.text] = curr.precedence;
+    return acc;
+}, {});
 
 const printOneOperand = (node, path, print, nodePath) => {
     const shouldPrintParentheses = Node.isFilterExpression(node.right);
@@ -37,6 +45,58 @@ const printInterpolatedString = (node, path, print, options) => {
     printedFragments.unshift(path.call(print, ...currentPath));
     printedFragments.unshift(quoteChar(options));
     return concat(printedFragments);
+};
+
+const printBinaryExpression2 = (node, path, print) => {
+    node[EXPRESSION_NEEDED] = false;
+    node[STRING_NEEDS_QUOTES] = true;
+
+    const isBinaryLeft = Node.isBinaryExpression(node.left);
+    const isBinaryRight = Node.isBinaryExpression(node.right);
+
+    const alreadyIndented = firstValueInAncestorChain(
+        path,
+        ALREADY_INDENTED,
+        false
+    );
+    if (!alreadyIndented && isBinaryRight) {
+        node.right[ALREADY_INDENTED] = true;
+    }
+
+    const precedence = operatorPrecedence[node.operator];
+    const leftPrecedence = isBinaryLeft
+        ? operatorPrecedence[node.left.operator]
+        : Number.MAX_SAFE_INTEGER;
+    const rightPrecedence = isBinaryRight
+        ? operatorPrecedence[node.right.operator]
+        : Number.MAX_SAFE_INTEGER;
+    const printedLeft = path.call(print, "left");
+    const printedRight = path.call(print, "right");
+
+    const parts = [];
+    const leftNeedsParens =
+        leftPrecedence < precedence || Node.isFilterExpression(node.left);
+    const rightNeedsParens =
+        rightPrecedence < precedence || Node.isFilterExpression(node.right);
+    if (leftNeedsParens) {
+        parts.push("(");
+    }
+    parts.push(printedLeft);
+    if (leftNeedsParens) {
+        parts.push(")");
+    }
+    const potentiallyIndented = [line, node.operator, " "];
+    if (rightNeedsParens) {
+        potentiallyIndented.push("(");
+    }
+    potentiallyIndented.push(printedRight);
+    if (rightNeedsParens) {
+        potentiallyIndented.push(")");
+    }
+    const rightHandSide = alreadyIndented
+        ? concat(potentiallyIndented)
+        : indent(concat(potentiallyIndented));
+    return group(concat([...parts, rightHandSide]));
 };
 
 const printBinaryExpression = (node, path, print) => {
@@ -83,7 +143,7 @@ const p = (node, path, print, options) => {
     if (Node.isBinaryConcatExpression(node) && node.wasImplicitConcatenation) {
         return printInterpolatedString(node, path, print, options);
     }
-    return printBinaryExpression(node, path, print);
+    return printBinaryExpression2(node, path, print);
 };
 
 module.exports = {
