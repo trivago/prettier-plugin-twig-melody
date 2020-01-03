d32611892c25565efa394101d90b88e5e636aa1b
tbartel <thomas.bartel@trivago.com>
2020-01-03 13:48:37 +0100

    Get range ignore to work Add test cases

--- src/util/publicFunctions.js
+++ src/util/publicFunctions.js
@@ -253,6 +253,14 @@ const normalizeWhitespace = whitespace => {
     // Normalize to one single space
     return [line];
 };
+// Split string by whitespace, but preserving the whitespace
+// "\n   Next\n" => ["", "\n   ", "Next", "\n", ""]
+const splitByWhitespace = s => s.split(/([\s\n]+)/gm);
+
+const unifyWhitespace = (s, replacement = " ") =>
+    splitByWhitespace(s)
+        .filter(s => !isWhitespaceOnly(s))
+        .join(replacement);
 
 const createTextGroups = (
     s,
