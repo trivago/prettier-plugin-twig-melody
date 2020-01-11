8939ac0da7e3cd3949ae0dd35a306747050d7469
tbartel <thomas.bartel@trivago.com>
2020-01-12 00:27:13 +0100

    No spaces around ".." operator

--- src/print/BinaryExpression.js
+++ src/print/BinaryExpression.js
@@ -1,11 +1,10 @@
 const prettier = require("prettier");
-const { group, join, concat, line, indent } = prettier.doc.builders;
+const { group, concat, line, softline, indent } = prettier.doc.builders;
 const { Node } = require("melody-types");
 const {
     EXPRESSION_NEEDED,
     STRING_NEEDS_QUOTES,
     INSIDE_OF_STRING,
-    wrapExpressionIfNeeded,
     firstValueInAncestorChain,
     quoteChar
 } = require("../util");
@@ -13,24 +12,12 @@ const { extension: coreExtension } = require("melody-extension-core");
 const ALREADY_INDENTED = Symbol("ALREADY_INDENTED");
 const IS_ROOT_BINARY_EXPRESSION = Symbol("IS_ROOT_BINARY_EXPRESSION");
 const OPERATOR_PRECEDENCE = Symbol("OPERATOR_PRECEDENCE");
+const NO_WHITESPACE_AROUND = [".."];
 
 const operatorPrecedence = coreExtension.binaryOperators.reduce((acc, curr) => {
     acc[curr.text] = curr.precedence;
     return acc;
 }, {});
-
-const printOneOperand = (node, path, print, nodePath) => {
-    const shouldPrintParentheses = Node.isFilterExpression(node.right);
-
-    return concat([
-        line,
-        node.operator,
-        " ",
-        shouldPrintParentheses ? "(" : "",
-        path.call(print, ...nodePath, "right"),
-        shouldPrintParentheses ? ")" : ""
-    ]);
-};
 
 const printInterpolatedString = (node, path, print, options) => {
     node[STRING_NEEDS_QUOTES] = false;
@@ -49,13 +36,18 @@ const printInterpolatedString = (node, path, print, options) => {
     return concat(printedFragments);
 };
 
-const printBinaryExpression2 = (node, path, print) => {
+const operatorNeedsSpaces = operator => {
+    return NO_WHITESPACE_AROUND.indexOf(operator) < 0;
+};
+
+const printBinaryExpression = (node, path, print) => {
     node[EXPRESSION_NEEDED] = false;
     node[STRING_NEEDS_QUOTES] = true;
 
     const isBinaryLeft = Node.isBinaryExpression(node.left);
     const isBinaryRight = Node.isBinaryExpression(node.right);
     const isLogicalOperator = ["and", "or", "not"].indexOf(node.operator) > -1;
+    const whitespaceAroundOperator = operatorNeedsSpaces(node.operator);
 
     const alreadyIndented = firstValueInAncestorChain(
         path,
@@ -104,7 +96,11 @@ const printBinaryExpression2 = (node, path, print) => {
     if (leftNeedsParens) {
         parts.push(")");
     }
-    const potentiallyIndented = [line, node.operator, " "];
+    const potentiallyIndented = [
+        whitespaceAroundOperator ? line : softline,
+        node.operator,
+        whitespaceAroundOperator ? " " : ""
+    ];
     if (rightNeedsParens) {
         potentiallyIndented.push("(");
     }
@@ -125,51 +121,11 @@ const printBinaryExpression2 = (node, path, print) => {
         : result;
 };
 
-const printBinaryExpression = (node, path, print) => {
-    node[EXPRESSION_NEEDED] = false;
-    node[STRING_NEEDS_QUOTES] = true;
-
-    let currentNode = node;
-    const pathToFinalLeftHandSide = ["left"];
-    const binaryExpressions = [printOneOperand(node, path, print, [])];
-
-    // Here, we do not do the usual recursion using path.call(), but
-    // instead traverse the chain of BinaryExpressions ourselves (in
-    // case there are multiple chained BinaryExpressions, that is).
-    // Reason: For a proper layout like this
-    // cond1 or
-    // cond2 or
-    // cond3 or
-    // cond4
-    // we need all the individual binary expressions in one group. This
-    // can only be achieved by collecting them manually in the top-level
-    // BinaryExpression node.
-    while (Node.isBinaryExpression(currentNode.left)) {
-        binaryExpressions.unshift(
-            printOneOperand(
-                currentNode.left,
-                path,
-                print,
-                pathToFinalLeftHandSide
-            )
-        );
-        pathToFinalLeftHandSide.push("left"); // Go one level deeper
-        currentNode = currentNode.left;
-        currentNode[EXPRESSION_NEEDED] = false;
-        currentNode[STRING_NEEDS_QUOTES] = true;
-    }
-    binaryExpressions.unshift(path.call(print, ...pathToFinalLeftHandSide));
-
-    wrapExpressionIfNeeded(path, binaryExpressions, node);
-
-    return group(join("", binaryExpressions));
-};
-
 const p = (node, path, print, options) => {
     if (Node.isBinaryConcatExpression(node) && node.wasImplicitConcatenation) {
         return printInterpolatedString(node, path, print, options);
     }
-    return printBinaryExpression2(node, path, print);
+    return printBinaryExpression(node, path, print);
 };
 
 module.exports = {
