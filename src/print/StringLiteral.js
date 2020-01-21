3a8d773b580d6ac5c2ae7366e1fbb49c28f94922
tbartel <thomas.bartel@trivago.com>
2020-01-21 09:55:22 +0100

    StringLiteral: Choose quote character dynamically depending on the string contents

--- src/print/StringLiteral.js
+++ src/print/StringLiteral.js
@@ -5,6 +5,34 @@ const {
     OVERRIDE_QUOTE_CHAR
 } = require("../util");
 
+const isUnmaskedOccurrence = (s, pos) => {
+    return pos === 0 || s[pos - 1] !== "\\";
+};
+
+const containsUnmasked = char => s => {
+    let pos = s.indexOf(char);
+    while (pos >= 0) {
+        if (isUnmaskedOccurrence(s, pos)) {
+            return true;
+        }
+        pos = s.indexOf(char, pos + 1);
+    }
+    return false;
+};
+
+const containsUnmaskedSingleQuote = containsUnmasked("'");
+const containsUnmaskedDoubleQuote = containsUnmasked('"');
+
+const getQuoteChar = (s, options) => {
+    if (containsUnmaskedSingleQuote(s)) {
+        return '"';
+    }
+    if (containsUnmaskedDoubleQuote(s)) {
+        return "'";
+    }
+    return quoteChar(options);
+};
+
 const p = (node, path, print, options) => {
     // The structure this string literal is part of
     // determines if we need quotes or not
@@ -13,6 +41,9 @@ const p = (node, path, print, options) => {
         STRING_NEEDS_QUOTES,
         false
     );
+    // In case of a string with interpolations, only double quotes
+    // are allowed. This is then indicated by OVERRIDE_QUOTE_CHAR
+    // in an ancestor.
     const overridingQuoteChar = firstValueInAncestorChain(
         path,
         OVERRIDE_QUOTE_CHAR,
@@ -22,7 +53,7 @@ const p = (node, path, print, options) => {
     if (needsQuotes) {
         const quote = overridingQuoteChar
             ? overridingQuoteChar
-            : quoteChar(options);
+            : getQuoteChar(node.value, options);
         return quote + node.value + quote;
     }
 
