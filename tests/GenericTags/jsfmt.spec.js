21d773609cd00294d3ddd52b6578bbaf168aef2c
tbartel <thomas.bartel@trivago.com>
2020-02-19 06:56:45 +0100

    Add test cases for custom Twig tags

---
+++ tests/GenericTags/jsfmt.spec.js
@@ -0,0 +1,8 @@
+run_spec(__dirname, ["melody"], {
+    twigMultiTags: [
+        "nav,endnav",
+        "switch,case,endswitch",
+        "ifchildren,endifchildren",
+        "cache,endcache"
+    ]
+});
